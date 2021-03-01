98bcb54d45a09003b8b86960f79f7db3590728c8
Will Clingan <wuz352@hotmail.com>
2021-02-28 19:32:47 -0600

    Update test8-insertion-sort.go

--- test8-insertion-sort.go
+++ test8-insertion-sort.go
@@ -1,9 +1,11 @@
 // INSERTION SORT
+// return arr unnecessary due to memory address assignment
+
 package main
 
 import "fmt"
 
-func insertionSort(x int, arr []int) []int {
+func insertionSort(x int, arr []int) {
 	for i := 0; i < x; i++ {
 		j := i
 		for j >= 0 && arr[j] > arr[j+1] {
@@ -11,15 +13,13 @@ func insertionSort(x int, arr []int) []int {
 			arr[j] = arr[j+1]
 			arr[j+1] = s
 			j--
-			// fmt.Println(arr)
 		}
-		fmt.Println(arr)
 	}
-	return arr
 }
 
 func main() {
 	num := 5
 	puzzle1 := []int{5, 6, 2, 4, 3, 1}
 	insertionSort(num, puzzle1)
+	fmt.Println(puzzle1)
 }
