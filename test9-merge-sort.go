15eadfdcf09103f15133b17274b97a5cc1172c87
Will Clingan <wuz352@hotmail.com>
2021-02-28 19:15:11 -0600

    Create test9-merge-sort.go

---
+++ test9-merge-sort.go
@@ -0,0 +1,35 @@
+// MERGE SORT
+
+package main
+
+import "fmt"
+
+func mergeSort(arr []int) {
+  // return arr unnecessary due to memory address assignment
+	if len(arr) > 1 {
+
+		mid := len(arr) / 2
+
+		L := arr[:mid]
+		R := arr[mid:]
+
+		mergeSort(L)
+		mergeSort(R)
+
+		for i := 0; i < len(L); i++ {
+			if L[i] > R[i] {
+				temp := L[i]
+				L[i] = R[i]
+				R[i] = temp
+			}
+		}
+
+	}
+}
+
+func main() {
+	puzzle1 := []int{12, 11, 13, 5, 7, 6}
+	fmt.Println(puzzle1)
+	mergeSort(puzzle1)
+	fmt.Println(puzzle1)
+}
