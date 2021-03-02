e6af5d6c70ca4fbffb1ba7213970afe4b0a8f00a
Will Clingan <wuz352@hotmail.com>
2021-03-01 21:13:03 -0600

    Create test11-quick-sort.go

---
+++ test11-quick-sort.go
@@ -0,0 +1,48 @@
+// QUICK SORT
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func partition(arr []int, x int, y int) int {
+	i := (x - 1)
+	pivot := arr[y]
+	for j := x; j < y; j++ {
+		if arr[j] <= pivot {
+			i++
+			arr[i], arr[j] = arr[j], arr[i]
+		}
+	}
+	arr[i+1], arr[y] = arr[y], arr[i+1]
+	return (i + 1)
+}
+
+func quickSort(arr []int, x int, y int) []int {
+	if len(arr) == 1 {
+		return arr
+	}
+	if x < y {
+		partInd := partition(arr, x, y)
+		quickSort(arr, x, partInd-1)
+		quickSort(arr, partInd+1, y)
+	}
+	return arr
+}
+
+func main() {
+	puzzle1 := []int{-14, -14, -13, -7, -4, -2, 0, 0, 5, 7, 7, 8, 12, 15, 15,
+		-2, 7, 15, -14, 0, 15, 0, 7, -7, -4, -13, 5, 8, -14, 12, 49, 6, 78, 99,
+		88, 48, 38, 29, 30, 133, 34, 52, 526, 664, 267, 377}
+	fmt.Println(puzzle1)
+	start := time.Now()
+	quickSort(puzzle1, 0, (len(puzzle1) - 1))
+	// start2 := time.Now()
+	// Resolves so fast time.Since() doesn't work
+	elapsed := time.Since(start)
+	fmt.Println(puzzle1)
+	fmt.Println("Start:", start)
+	fmt.Println("Elapsed:", elapsed)
+}
