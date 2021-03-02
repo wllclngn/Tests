92222aa936738439256ef672259c0ac95f321742
Will Clingan <wuz352@hotmail.com>
2021-03-01 22:36:42 -0600

    Update test11-quick-sort.go

--- test11-quick-sort.go
+++ test11-quick-sort.go
@@ -17,23 +17,21 @@ func partition(arr []int, x int, y int) int {
 	return (i + 1)
 }
 
-func quickSort(arr []int, x int, y int) []int {
-	if len(arr) == 1 {
-		return arr
-	}
-	if x < y {
+func quickSort(arr []int, x int, y int) {
+	if len(arr) <= 1 {
+		panic("The input slice size is one or empty.")
+	} else if x < y {
 		partInd := partition(arr, x, y)
 		quickSort(arr, x, partInd-1)
 		quickSort(arr, partInd+1, y)
 	}
-	return arr
 }
 
 func main() {
-	puzzle1 := []int{-14, -14, -13, -7, -4, -2, 0, 0, 5, 7, 7, 8, 12, 15, 15,
+	slice := []int{-14, -14, -13, -7, -4, -2, 0, 0, 5, 7, 7, 8, 12, 15, 15,
 		-2, 7, 15, -14, 0, 15, 0, 7, -7, -4, -13, 5, 8, -14, 12, 49, 6, 78, 99,
 		88, 48, 38, 29, 30, 133, 34, 52, 526, 664, 267, 377}
-	fmt.Println(puzzle1)
-	quickSort(puzzle1, 0, (len(puzzle1) - 1))
-	fmt.Println(puzzle1)
+	fmt.Println(slice)
+	quickSort(slice, 0, (len(slice) - 1))
+	fmt.Println(slice)
 }
