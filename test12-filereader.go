b017b351ffa9ce71a2b2335d5653a11126d16757
Will Clingan <wuz352@hotmail.com>
2021-03-19 11:05:41 -0500

    Update test12-filereader.go

--- test12-filereader.go
+++ test12-filereader.go
@@ -1,1 +1,19 @@
+package main
+import (
+    "fmt"
+    "strings"
+    "io/ioutil"
+)
 
+func main() {
+    data, err := ioutil.ReadFile("[FILE]")
+    if err != nil {
+        fmt.Println("File reader ERROR:", err)
+        return
+    }
+    data_del := string(data[:])
+    data_del = strings.Replace(data_del, "\n", "", -1)
+    //data_del = strings.TrimSpace(data_del)
+    fmt.Println("READ IN DATA:")
+    fmt.Println(data_del)
+}
