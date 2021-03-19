dc3902f1d175ebb7e907c8ea160e5c6d9f27686c
Will Clingan <wuz352@hotmail.com>
2021-03-19 12:22:14 -0500

    Update test12-filereader.go

--- test12-filereader.go
+++ test12-filereader.go
@@ -11,7 +11,7 @@ func main() {
         fmt.Println("File input ERROR:", err)
         return
     }
-    data_del := string(data[:])
-    data_del = strings.Replace(data_del, "\n", "", -1)
-    fmt.Println("INPUT DATA:", data_del)
+    data_str := string(data[:])
+    data_str = strings.Replace(data_str, "\n", "", -1)
+    fmt.Println("INPUT DATA:", data_str)
 }
