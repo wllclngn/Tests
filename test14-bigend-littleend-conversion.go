5e0bb187e813d0512c75aaafd7c6bd1b9fc5b975
Will Clingan <wuz352@hotmail.com>
2021-03-22 13:29:31 -0500

    Update test14-bigend-littleend-conversion.go

--- test14-bigend-littleend-conversion.go
+++ test14-bigend-littleend-conversion.go
@@ -11,34 +11,34 @@ func bigLil() {
 
 }
 
-func lilBig(x []byte) int {
+func lilBig(x []byte) []byte {
+
+	var byteR []byte
 
 	if (len(x) % 4) != 0 {
-		fmt.Println("ERROR: ", x)
-		panic("Please check the length of your bits.")
+		fmt.Println("ERROR:", x)
+		panic("Bit's length is not modulus of four.")
 	}
 
-	var num int
-
-	return num
+	return byteR
 }
 
 func main() {
-	//var nibble []byte
-	nibble := make([]byte, 2, 2)
-	nibble = []byte{0, 1}
-	lilBig(nibble)
+	/*
+		nibble := make([]byte, 2, 2)
+		nibble = []byte{0, 1}
+		lilBig(nibble)
+	*/
+
+	bite := make([]byte, 8, 8)
+	bite = []byte{0, 0, 0, 1, 0, 0, 1, 1}
+	lilBig(bite)
+
+	bigBite := make([]byte, 8, 8)
+	bigBite = []byte{0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1}
+	lilBig(bigBite)
 
 	/*
-		var bite []byte
-		bite = make([]byte, 8, 8)
-		bite == []byte{0, 0, 0, 1, 0, 0, 1, 1}
-		lilBig(bite)
-
-		var bite []byte
-		bite = make([]byte, 8, 8)
-		bite == []byte{0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1}
-		lilBig(bite)
 
 		testLilBig2 := lilBig(bite)
 		testLilBig3 := lilBig(bigBite)
