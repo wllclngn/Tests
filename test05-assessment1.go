79e7d308cc8409c2c44f90aa071384c65774aaf3
Will Clingan <wuz352@hotmail.com>
2021-03-23 07:00:48 -0500

    Update test05-assessment1.go

--- test05-assessment1.go
+++ test05-assessment1.go
@@ -10,16 +10,16 @@ import (
 func solution(A int, B int) int {
 	x := strconv.Itoa(A)
 	y := strconv.Itoa(B)
-	for i := 0; i <= len(y)-1; i++ {
+	for i := 0; i < len(y); i++ {
 		if x[0] == y[i] {
-			if i+1 <= len(y)-1 {
+			if i+1 < len(y) {
 				if x[1] == y[i+1] {
 					// var z [1000000]string
 					z := make([]string, len(y))
 					for j := range z {
 						z[j] = "0"
 					}
-					for k := i + 2; k <= len(y)-1; k++ {
+					for k := i + 2; k < len(y); k++ {
 						z[k] = string(y[k])
 					}
 					zJoined := strings.Join(z, "")
@@ -36,15 +36,15 @@ func solution(A int, B int) int {
 }
 
 func extracurricular(C string, D string, index int) int {
-	for l := index; l <= len(D)-1; l++ {
+	for l := index; l < len(D); l++ {
 		if C[0] == D[l] {
-			if l+1 <= len(D)-1 {
+			if l+1 < len(D) {
 				if C[1] == D[l+1] {
 					z := make([]string, len(D))
 					for m := range z {
 						z[m] = "0"
 					}
-					for n := l + 2; n <= len(D)-1; n++ {
+					for n := l + 2; n < len(D); n++ {
 						z[n] = string(D[n])
 					}
 					zJoined := strings.Join(z, "")
