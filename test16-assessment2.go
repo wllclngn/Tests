6ba41bd40d47692ec806592ddf81485b9d8bf32d
Will Clingan <wuz352@hotmail.com>
2021-03-23 09:56:16 -0500

    Update test16-assessment2.go

--- test16-assessment2.go
+++ test16-assessment2.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"strings"
 )
-
 func dig(x string) string {
 
 	newString := []string{}
 
 	for i := 0; i < len(x); i++ {
 		if string(x[i]) == "<" {
-			if i < len(x)-1 {
+			if i == len(x)-1 {
+				newString = append(newString, "<")
+				newString = append(newString, ">")
+			} else {
 				if string(x[i+1]) == ">" {
 					newString = append(newString, "<")
 					newString = append(newString, ">")
@@ -22,25 +24,30 @@ func dig(x string) string {
 				}
 			}
 		} else {
-			if i < len(x)-1 {
-				newString = append(newString, "<")
-				newString = append(newString, ">")
-			}
+			newString = append(newString, "<")
+			newString = append(newString, ">")
 		}
 	}
 	newString2 := strings.Join(newString, "")
-	fmt.Println(newString2)
+	newlen := len(newString2) / 2
+	fmt.Println(newlen, " ", newString2)
 	return newString2
 }
 
 func main() {
-	this := "><><<>>>>>"
-	fmt.Println(this)
-	dig(this)
-	this2 := ">>>>>>>>"
-	fmt.Println(this2)
-	dig(this2)
-	this3 := "<<<<<<<<"
-	fmt.Println(this3)
-	dig(this3)
+	/*
+		this := "><><<>>>>>"
+		fmt.Println(this)
+		dig(this)
+		this2 := ">>>>>>>>"
+		fmt.Println(this2)
+		dig(this2)
+		this3 := "<<<<<<<<"
+		fmt.Println(this3)
+		dig(this3)
+	*/
+	this4 := "<<<<<<<<<<<<<<<<<>><><>>><>><<<<<<<<>>><<<><>>><<"
+	lenthis4 := len(this4)
+	fmt.Println(this4, " ", lenthis4)
+	dig(this4)
 }
