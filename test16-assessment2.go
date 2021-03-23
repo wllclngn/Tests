7a163dc65b43b52baa5906a95e6b9e36feaa8fda
Will Clingan <wuz352@hotmail.com>
2021-03-23 10:09:39 -0500

    Update test16-assessment2.go

--- test16-assessment2.go
+++ test16-assessment2.go
@@ -8,6 +8,7 @@ import (
 func dig(x string) string {
 
 	newString := []string{}
+	completions := 0
 
 	for i := 0; i < len(x); i++ {
 		if string(x[i]) == "<" {
@@ -16,6 +17,7 @@ func dig(x string) string {
 			} else {
 				if string(x[i+1]) == ">" {
 					newString = append(newString, "<>")
+					completions++
 					i++
 				} else {
 					newString = append(newString, "<>")
@@ -26,21 +28,26 @@ func dig(x string) string {
 		}
 	}
 	newString2 := strings.Join(newString, "")
-	fmt.Println(newString2 + "\n")
+	newLen := len(newString2)
+	fmt.Println("COMPLETE #s:", completions, "LEN:", newLen, "\nNEW:"+newString2+"\n")
 	return newString2
 }
 
 func main() {
 	this := "><><<>>>>>"
-	fmt.Println(this)
+	thisLen := len(this)
+	fmt.Println("LEN:", thisLen, "\nORIGINAL:", this)
 	dig(this)
 	this2 := ">>>>>>>>"
-	fmt.Println(this2)
+	thisLen2 := len(this2)
+	fmt.Println("LEN:", thisLen2, "\nORIGINAL:", this2)
 	dig(this2)
 	this3 := "<<<<<<<<"
-	fmt.Println(this3)
+	thisLen3 := len(this3)
+	fmt.Println("LEN:", thisLen3, "\nORIGINAL:", this3)
 	dig(this3)
 	this4 := "<<<<<<<<<<<<<<<<<>><><>>><>><<<<<<<<>>><<<><>>><<"
-	fmt.Println(this4)
+	thisLen4 := len(this4)
+	fmt.Println("LEN:", thisLen4, "\nORIGINAL:", this4)
 	dig(this4)
 }
