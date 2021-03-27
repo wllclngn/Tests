097045b6431009c91a15d0261ce6f9627bc9a96b
Will Clingan <wuz352@hotmail.com>
2021-03-27 11:54:28 -0500

    Create test20-binary-search-alpha.go

---
+++ test20-binary-search-alpha.go
@@ -0,0 +1,47 @@
+// BINARY SEARCH, ALPHABET
+
+package main
+
+import "fmt"
+import "strings"
+
+func binSearch(x string, y []string) int {
+    low := 0
+    high := len(y) - 1
+
+    for low <= high {
+        point := low + (high-low)/2
+        xl := strings.ToLower(x)
+        m := strings.ToLower(y[point])
+
+        switch {
+            case xl == m:
+                return point
+            case xl < m:
+                high = point - 1
+            default:
+                low = point + 1
+        }
+    }
+    return -1
+}
+
+func main() {
+    var searched []string = []string{
+        "aardvark",
+        "anteater",
+        "chimpanzee",
+        "hippo",
+        "marsupial",
+        "orangutan",
+        "rhino",
+    }
+    fmt.Println("Sorted slice:", searched)
+    sought := "rhino"
+    i := binSearch(sought, searched)
+    if i < 0 {
+      fmt.Println("The word", sought, "could not be found!")
+    } else {
+      fmt.Println("The word", sought, "was found at index:", i, searched[i])
+    }
+}
