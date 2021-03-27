867787d469ef6743975dab8cf7a28bbf575fcbdf
Will Clingan <wuz352@hotmail.com>
2021-03-27 14:49:37 -0500

    Update test01-dectohex.go

--- test01-dectohex.go
+++ test01-dectohex.go
@@ -13,32 +13,33 @@ func dectohex(x int) {
 	value2 := []string{"0x"}
 	if x > 9 {
 		for i := 0; i < 9223372036854775807; i++ {
-			if x >= 16 {
+		    if x >= 16 {
 				y := x % 16
 				value = append(value, y)
 				x = x / 16
-			} else {
+		   	} else {
 				value = append(value, x)
 				break
 			}
 		}
 		for j := len(value) - 1; j >= 0; j-- {
-			if value[j] == 10 {
-				value2 = append(value2, "A")
-			} else if value[j] == 11 {
-				value2 = append(value2, "B")
-			} else if value[j] == 12 {
-				value2 = append(value2, "C")
-			} else if value[j] == 13 {
-				value2 = append(value2, "D")
-			} else if value[j] == 14 {
-				value2 = append(value2, "E")
-			} else if value[j] == 15 {
-				value2 = append(value2, "F")
-			} else {
-				valueStr := strconv.Itoa(value[j])
-				value2 = append(value2, valueStr)
-			}
+		    switch {
+			case value[j] == 10:
+					    value2 = append(value2, "A")
+			case value[j] == 11:
+					    value2 = append(value2, "B")
+			case value[j] == 12:
+					    value2 = append(value2, "C")
+			case value[j] == 13:
+					    value2 = append(value2, "D")
+			case value[j] == 14:
+					    value2 = append(value2, "E")
+			case value[j] == 15:
+					    value2 = append(value2, "F")
+			default:
+			    valueStr := strconv.Itoa(value[j])
+			    value2 = append(value2, valueStr)
+		    }
 		}
 	} else {
 		z := strconv.Itoa(x)
