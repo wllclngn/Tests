0d383898c7e07de1769c90962ebe087ccf80140f
Will Clingan <wuz352@hotmail.com>
2021-03-31 14:23:45 -0500

    Update test20-binary-search-alpha.go

--- test20-binary-search-alpha.go
+++ test20-binary-search-alpha.go
@@ -2,44 +2,46 @@
 
 package main
 
-import "fmt"
-import "strings"
+import (
+	"fmt"
+	"strings"
+)
 
 func binSearch(x string, y []string) int {
-    high := len(y) - 1
-    low := 0
+	high := len(y) - 1
+	low := 0
 
-    for high >= low {
-        point := ((high-low)/2) + low
+	for high >= low {
+		point := ((high - low) >> 1) + low
 
-        switch {
-            case strings.EqualFold(x, y[point]):
-                return point
-            case strings.Compare(x, y[point]) == -1:
-                high = point - 1
-            case strings.Compare(x, y[point]) == 1:
-                low = point + 1
-        }
-    }
-    return -1
+		switch {
+		case strings.EqualFold(x, y[point]):
+			return point
+		case strings.Compare(x, y[point]) == -1:
+			high = point - 1
+		case strings.Compare(x, y[point]) == 1:
+			low = point + 1
+		}
+	}
+	return -1
 }
 
 func main() {
-    var searched []string = []string{
-        "aardvark",
-        "anteater",
-        "chimpanzee",
-        "hippo",
-        "marsupial",
-        "orangutan",
-        "rhino",
-    }
-    fmt.Println("SORTED SLICE LIBRARY:", searched)
-    sought := "HIPPO"
-    i := binSearch(sought, searched)
-    if i > 0 {
-        fmt.Println("SEARCH:", sought, "\nINDEX RESULT:", i, "\nSLICE LIBRARY MATCH:", searched[i])
-    } else {
-        fmt.Println(sought, "was not found in the slice's library!")
-    }
+	var searched []string = []string{
+		"aardvark",
+		"anteater",
+		"chimpanzee",
+		"hippo",
+		"marsupial",
+		"orangutan",
+		"rhino",
+	}
+	fmt.Println("SORTED SLICE LIBRARY:", searched)
+	sought := "HIPPO"
+	i := binSearch(sought, searched)
+	if i > 0 {
+		fmt.Println("SEARCH:", sought, "\nINDEX RESULT:", i, "\nSLICE LIBRARY MATCH:", searched[i])
+	} else {
+		fmt.Println(sought, "was not found in the slice's library!")
+	}
 }
