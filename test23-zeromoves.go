2107804d8e09762f06c59fdf114a1d3ca9407ac6
Will Clingan <wuz352@hotmail.com>
2021-04-03 17:16:53 -0500

    Update test23-zeromoves.go

--- test23-zeromoves.go
+++ test23-zeromoves.go
@@ -5,6 +5,7 @@ package main
 import (
 	"fmt"
 	"math/rand"
+	"time"
 )
 
 func zeroMover(board []int) {
@@ -14,19 +15,15 @@ func zeroMover(board []int) {
 			board[i], board[i+1] = board[i+1], board[i]
 			continue
 		} else if i < len(board)-2 && board[i] == 0 && board[i+1] == 0 {
-			j := zerored(board, i+2)
-			board[i], board[j] = board[j], board[i]
-		}
-	}
-}
 
-func zerored(board []int, x int) int {
-	for ; x < len(board)-1; x++ {
-		if board[x] != 0 {
-			break
+			for j := i; j < len(board)-1; j++ {
+				if board[j] != 0 {
+					board[i], board[j] = board[j], board[i]
+					break
+				}
+			}
 		}
 	}
-	return x
 }
 
 func main() {
@@ -35,6 +32,9 @@ func main() {
 		board[i] = rand.Intn(2)
 	}
 	fmt.Println(board)
+	start := time.Now()
 	zeroMover(board)
+	elapsed := time.Since(start)
 	fmt.Println(board)
+	fmt.Println(elapsed)
 }
