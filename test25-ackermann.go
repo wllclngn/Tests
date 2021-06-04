e33aaabad70274a3560796d2fa596828d203d655
Will Clingan <wuz352@hotmail.com>
2021-06-04 10:58:03 -0500

    Update test25-ackermann.go

--- test25-ackermann.go
+++ test25-ackermann.go
@@ -1,13 +1,12 @@
 package main
 
-import "fmt"
+// import "fmt"
 
 func ack(x int, y int) int {
 	// fmt.Println(x, y, "are the current values.")
 	if x == 0 {
 		return y + 1
-	}
-	if y == 0 {
+	} else if y == 0 {
 		return ack(x-1, 1)
 	}
 	return ack(x-1, ack(x, y-1))
