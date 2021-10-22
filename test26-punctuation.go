4fcd2919cafe243a65084f1da3d92dbeb9f53d79
Will Clingan <wuz352@hotmail.com>
2021-10-22 07:43:33 -0500

    Update test26-punctuation.go

--- test26-punctuation.go
+++ test26-punctuation.go
@@ -17,7 +17,6 @@ func punctuation(x string) []string {
 		testing := strconv.QuoteRuneToASCII(rune(x[i]))
 		if len(testing) == 8 {
 			noQuote := testing[2 : len(testing)-1]
-			fmt.Println(noQuote)
 			switch {
 
 			case noQuote == "u0094":
@@ -33,63 +32,47 @@ func punctuation(x string) []string {
 
 		switch {
 		case x[i] == 33:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "!")
 		case x[i] == 34:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "\"")
 		case x[i] == 35:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "#")
 		case x[i] == 36:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "$")
 		case x[i] == 37:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "%")
 		case x[i] == 38:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "&")
 		case x[i] == 39:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "'")
 		case x[i] == 40:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "(")
 		case x[i] == 41:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, ")")
 		case x[i] == 42:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "*")
 		case x[i] == 43:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "+")
 		case x[i] == 44:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, ",")
 		case x[i] == 45:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "-")
 		case x[i] == 46:
-			justPunct = append(justPunct, string(x[i]))
-		case x[i] == 37:
-			justPunct = append(justPunct, string(x[i]))
-		case x[i] == 42:
-			justPunct = append(justPunct, string(x[i]))
-		case x[i] == 46:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, ".")
 		case x[i] == 47:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "/")
 		case x[i] == 58:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, ":")
 		case x[i] == 59:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, ";")
 		case x[i] == 61:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "=")
 		case x[i] == 63:
-			justPunct = append(justPunct, string(x[i]))
-		case x[i] == 94:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "?")
 		case x[i] == 96:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "`")
 		case x[i] == 126:
-			justPunct = append(justPunct, string(x[i]))
-		case x[i] == 128:
-			justPunct = append(justPunct, string(x[i]))
-		case x[i] == 133:
-			justPunct = append(justPunct, string(x[i]))
-		case x[i] == 150:
-			justPunct = append(justPunct, string(x[i]))
-		case x[i] == 182:
-			justPunct = append(justPunct, string(x[i]))
+			justPunct = append(justPunct, "~")
 		}
 	}
 	return justPunct
@@ -103,6 +86,8 @@ func main() {
 		return
 	}
 	data_str := string(data[:])
-	data_str = strings.Replace(data_str, "\r", " ", -1)
+	data_str = strings.Replace(data_str, "\r", "", -1)
+	data_str = strings.Replace(data_str, "\n", "", -1)
+	data_str = strings.Replace(data_str, " ", "", -1)
 	fmt.Println(punctuation(data_str))
 }
